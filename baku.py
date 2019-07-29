e5428590a496654ea837c6a383e42b52ac8b9333
Martin Alonso <mtnalonso@protonmail.com>
2019-07-29 17:07:19 +0200

    Ability to reorder backups

--- baku.py
+++ baku.py
@@ -1,15 +1,22 @@
 import argparse
-import datetime
+from datetime import datetime
 import os
+import shutil
 
 import pysftp
 
 import creds
 
 
+DEST_PATH = '{}/{}'.format(os.getcwd(), creds.DEST_PATH)
+LAST_BACKUP_FILENAME = 'last.tar.gz'
+
+
 def load_args():
     parser = argparse.ArgumentParser(prog='baku.py')
     parser.add_argument('-c', '--cron', action='store_true')
+    parser.add_argument('-f', '--force', action='store_true')
+    parser.add_argument('-s', '--sync', action='store_true')
     return parser.parse_args()
 
 
@@ -19,15 +26,13 @@ def run_cron_task():
     host = creds.HOST
     directory = creds.PATH
     filename = creds.FILENAME
-    dest_path = creds.DEST_PATH
 
-    backup_date = datetime.datetime.now().strftime("%Y-%m-%d")
-    dest_path = '{}/{}'.format(os.getcwd(), dest_path)
+    backup_date = datetime.now().strftime("%Y-%m-%d")
 
     if not os.path.exists(dest_path):
         os.makedirs(dest_path)
 
-    dest_path = '{}{}_{}'.format(dest_path, backup_date, filename)
+    dest_path = '{}{}_{}'.format(DEST_PATH, backup_date, filename)
 
     cnopts = pysftp.CnOpts()
     cnopts.hostkeys = None
@@ -37,6 +42,57 @@ def run_cron_task():
             print('[*] Downloading {}...'.format(filename))
             sftp.get(filename, localpath=dest_path)
             print('[+] Downloaded {}'.format(filename))
+
+    shutil.copy2(dest_path, DEST_PATH + LAST_BACKUP_FILENAME)
+    return
+
+
+def validate_backup_file():
+    # TODO: this function will call a validator to check the file integrity
+    pass
+
+
+def reorder_backup_files():
+    today = datetime.today()
+    reorder_yearly_backup_files(today)
+    reorder_monthly_backup_files(today)
+    reorder_weekly_backup_files(today)
+    reorder_daily_backup_files(today)
+
+
+def reorder_yearly_backup_files(today):
+    current_day_of_year = today.timetuple().tm_yday
+    if current_day_of_year == 1:
+        yearly_filename = 'yearly-{}.tar.gz'.format(today.year)
+        print('creating {}'.format(yearly_filename))
+        shutil.copy2(DEST_PATH + LAST_BACKUP_FILENAME, yearly_filename)
+    return 
+
+
+def reorder_monthly_backup_files(today):
+    current_day_of_month = today.day
+    if current_day_of_month == 1:
+        monthly_filename = 'monthly-{}.tar.gz'.format(today.month)
+        print('creating {}'.format(monthly_filename))
+        shutil.copy2(DEST_PATH + LAST_BACKUP_FILENAME, monthly_filename)
+    return 
+
+
+def reorder_weekly_backup_files(today):
+    current_day_of_week = today.isoweekday()
+    if current_day_of_week == 1:
+        weekly_filename = 'weekly-{}.tar.gz'.format(str(today.date()))
+        print('creating {}'.format(weekly_filename))
+        shutil.copy2(DEST_PATH + LAST_BACKUP_FILENAME, weekly_filename)
+    return
+
+
+def reorder_daily_backup_files(today):
+    daily_files = os.listdir(DEST_PATH + 'daily*')
+    if len(daily_files) > 14:
+        oldest_backup = min(daily_files, key=os.path.getctime)
+        print('removing {}{}'.format(DEST_PATH, oldest_backup))
+        os.remove(DEST_PATH + oldest_backup)
     return
 
 
@@ -45,3 +101,8 @@ if __name__ == '__main__':
 
     if args.cron:
         run_cron_task()
+        reorder_backup_files()
+    elif args.sync:
+        reorder_backup_files()
+    elif args.force:
+        raise NotImplementedError('Forced backup not implemented yet')
